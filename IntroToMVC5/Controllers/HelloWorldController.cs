10fcef401d34dd31c9d9f7d3368bd2d46c3959cf
Elena Kashevarova <kashevarova.dev@gmail.com>
2018-12-30 21:58:35 +0200

    Work with Controllers. Pass data from controller to view

---
+++ IntroToMVC5/Controllers/HelloWorldController.cs
@@ -0,0 +1,36 @@
+﻿using IntroToMVC5.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IntroToMVC5.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // GET: HelloWorld
+        public ActionResult Index()
+        {
+            ViewBag.Greetings = "Hello From HelloWorldController";
+            ViewBag.Name = "Lena";
+            ViewBag.Country = "Ukraine";
+            Authors a = new Authors();
+            a.AuthorName = "Lena";
+            a.AuthorCountry="Ukraine";
+
+            return View(a);
+        }
+
+        public ActionResult QueryStringActions(string Message = "Default message in QueryStringActions method")
+        { 
+            ViewBag.Greetings = Message;
+            return View();
+        }
+
+        public ActionResult GoToURL(string url = "http://www.google.com")
+        {
+            return Redirect(url);
+        }
+    }
+}
---
+++ IntroToMVC5/Models/Authors.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IntroToMVC5.Models
+{
+    public class Authors
+    {
+        public string AuthorName { get; set; }
+        public string AuthorCountry { get; set; }
+    }
+}
